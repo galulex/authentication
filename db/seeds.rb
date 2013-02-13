9fbdbbe223799ab29c8c5b8ba6ecbed0f37ed72c
Alex Galushka <agalushka@svitla.com>
2013-02-13 19:07:13 +0200

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,14 +4,14 @@ require 'factory_girl_rails'
 
 admin = FactoryGirl.create(:admin, first_name: 'AppZone', last_name: 'Admin', email: 'marketplace@partnerpedia.com')
 puts admin.name
-9.times do |i|
+20.times do
   FactoryGirl.reload
-  logo = File.new(Dir.glob('spec/support/company_logos/*')[i])
+  logo = File.new(Dir.glob(Rails.root + 'spec/support/company_logos/*').sample)
   user = FactoryGirl.create(:user, company: FactoryGirl.build(:company, logo: logo))
   puts user.name
   rand(3).times do
-    icon = File.new(Dir.glob('spec/support/company_logos/*')[i])
-    image = File.new(Dir.glob('spec/support/product_images/*')[i])
+    icon = File.new(Rails.root + Dir.glob('spec/support/company_logos/*').sample)
+    image = File.new(Rails.root + Dir.glob('spec/support/product_images/*').sample)
     product = FactoryGirl.create(:product, company: user.company, icon: icon, image: image)
     puts product.name
   end
