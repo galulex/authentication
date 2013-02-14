de56239ed8a1bd5a44662696416dd1e9041db19e
Alex Galushka <agalushka@svitla.com>
2013-02-14 17:25:56 +0200

    all_logins

--- app/controllers/all_logins_controller.rb
+++ app/controllers/all_logins_controller.rb
@@ -1,15 +1,26 @@
 class AllLoginsController < ApplicationController
-  layout 'all_logins'
+  layout false
 
   def index
-    #company = current_user.company
-    @companies = Company.all
+    if params[:char]
+      @companies = Company.where('name LIKE ?', "#{params[:char]}%").page(params[:page]).per(5)
+    else
+      @companies = Company.page(params[:page]).per(5)
+    end
   end
 
   def show
     user = User.find(params[:id])
     cookies[:auth_token] = user.auth_token
     redirect_to :root
+  end
+
+  def search(search)
+    if search
+      find(:all, :conditions => ['name LIKE ?', "%#{search}%"])
+    else
+      find(:all)
+    end
   end
 
 end
