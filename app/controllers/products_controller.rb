e890389bb3199e8d76579234887a266a193a1d5a
Alex Galushka <agalushka@svitla.com>
2013-02-15 18:23:30 +0200

    guest fix

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,7 +4,9 @@ class ProductsController < ApplicationController
 
   def show
     @product = Product.where(id: params[:id]).includes(reviews: :user).first
-    @reviewed = @product.reviews.find_by_user_id(current_user.id)
+    if current_user
+      @reviewed = @product.reviews.find_by_user_id(current_user.id)
+    end
   end
 
 end
